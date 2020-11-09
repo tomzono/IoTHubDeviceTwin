8b8aa9c2932ab47451919ebad1ed7924e97d7b85
tomzono <68574400+tomzono@users.noreply.github.com>
2020-11-09 17:45:11 +0900

    add send Twin

--- DeviceTwin_Test/main.py
+++ DeviceTwin_Test/main.py
@@ -59,6 +59,11 @@ def iothub_SendMessage(str):
         twin = client.get_twin()
         print("Twin document:")
         print("{}".format(twin))
+        
+        reported_properties = {"temperature": random.randint(320, 800) / 10}
+        print("Setting reported temperature to {}".format(reported_properties["temperature"]))
+        device_client.patch_twin_reported_properties(reported_properties)
+
         result = True
     except Exception as e:
         print("Failed to send data to IoTHub")
