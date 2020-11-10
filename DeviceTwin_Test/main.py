4b8c10d4dd29904d5fa93e124c95855595922804
tomzono <68574400+tomzono@users.noreply.github.com>
2020-11-10 13:42:47 +0900

    okokok

--- DeviceTwin_Test/main.py
+++ DeviceTwin_Test/main.py
@@ -58,7 +58,7 @@ def iothub_SendMessage(str):
     twin = client.get_twin()
     respose_interval = twin['desired']['intervaal']
     print("Twin document:")
-    print(response)
+    print(response_interval)
     VANTIQ_FORWARD_HANDLING_DATA_COUNT = response_interval
     reported_properties = {"temperature": random.randint(320, 800) / 10,"device":device_id}
     print("Setting reported temperature to {}".format(reported_properties))
