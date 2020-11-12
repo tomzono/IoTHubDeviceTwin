83d84403ae686f457539ce46c10cc64a742dc5dd
tomzono <68574400+tomzono@users.noreply.github.com>
2020-11-12 14:07:42 +0900

    fix report

--- DeviceTwin_Test/main.py
+++ DeviceTwin_Test/main.py
@@ -59,8 +59,8 @@ def iothub_SendMessage(str):
         VANTIQ_FORWARD_HANDLING_DATA_COUNT = twin['desired']['intervaal']
         print ("VANTIQ_FORWARD_HANDLING_DATA_COUNT : {}".format(VANTIQ_FORWARD_HANDLING_DATA_COUNT))
         #test_send DeviceTwin_reported
-        reported_properties = {"temperature": random.randint(320, 800) / 10,"device":device_id}
-        print ("Setting reported temperature to {}".format(reported_properties))
+        #reported_properties = {"temperature": random.randint(320, 800) / 10,"device":device_id}
+        reported_properties = message
         client.patch_twin_reported_properties(reported_properties)
         #send Message to IoTHub
         client.send_message(message)
